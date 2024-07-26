79efd44d28f603601aff6f470f72f34f09322344
Shweta Varadarajan <83686575+shwetasvgit@users.noreply.github.com>
2024-07-26 11:25:46 +0000

    delete env

--- src/distilgpt2_api/api.py
+++ src/distilgpt2_api/api.py
@@ -1,32 +1,38 @@
-from fastapi import FastAPI
-from .text_generation import TextGenerator
-from functools import cache
 import logging
+from functools import cache
+
+from fastapi import FastAPI
+
+from .text_generation import TextGenerator
 
 app = FastAPI()
 
-@app.get('/')
+
+@app.get("/")
 async def health():
-    return { "health":"ok"}
+    return {"health": "ok"}
+
 
 @cache
 def get_model() -> TextGenerator:
     logging.info("Loading DistilGPT2 model")
     return TextGenerator()
 
+
 @app.on_event("startup")
 async def on_startup():
     get_model()
 
+
 @app.get("/{prompt}")
 async def generate_text(
-    prompt:str,
+    prompt: str,
     max_new_tokens: int = 50,
     num_return_sequences: int = 1,
 ) -> dict:
     model = get_model()
-    sequences = model.generate(prompt,max_new_tokens=max_new_tokens,num_return_sequences=num_return_sequences)
+    sequences = model.generate(
+        prompt, max_new_tokens=max_new_tokens, num_return_sequences=num_return_sequences
+    )
 
     return {"generated_sequences": sequences}
-
-
